f7bbdb2d98ce1cac3abda5f53576e03e3f142ae1
Fu-En.Wang <fulton84717@gmail.com>
2017-09-12 05:35:42 +0800

    gg

--- stitch.py
+++ stitch.py
@@ -1,37 +1,30 @@
 import cv2
 import numpy as np
 import functools
+import MovingLSQ as MLSQ
 import scipy.ndimage as nd
 
 refPT = np.array([
             [0, 0],
-            [719, 0],
-            [240, 210],
-            [479, 210],
             [360, 0],
+            [719, 0],
+            [600, 105],
+            [479, 210],
             [360, 210],
-            [120, 105],
-            [600, 105]
+            [240, 210],
+            [120, 105]
         ])
 
 fromPT = np.array([
             [0, 0],
-            [719, 0],
-            [87, 452],
-            [632, 452],
             [360, 0],
+            [719, 0],
+            [675, 226],
+            [632, 452],
             [360, 452],
-            [43, 226],
-            [675, 226]
+            [87, 452],
+            [43, 226]
          ])
-
-def Reproject(coordinates):
-    global H_inv
-    print coordinates
-    p = np.dot(H_inv, [coordinates[0], coordinates[1], 1])
-    p = p[:2] / p[2]
-    print p
-    return (p[0], p(1))
 
 def SolveH(srcPts, dstPts):
     src = np.ones([8, 3], np.float)
@@ -64,21 +57,57 @@ def SolveH(srcPts, dstPts):
 if __name__ == '__main__':
     ref = np.zeros([720, 720, 3], np.uint8)
     origin = cv2.imread('/Users/fu-en.wang/Project/BirdViewStitch/ImageStitching/img/birdview2/img-1.png', cv2.IMREAD_COLOR)
-    
-    H, pts = SolveH(fromPT, refPT)
 
+    mask = np.zeros([720, 720], np.float32)
+    cv2.fillConvexPoly(mask, refPT, color=1)
+    #mask_ref = mask
+    mask_ref = mask.astype(bool)
+    mask = np.zeros([720, 720], np.float32)
+    cv2.fillConvexPoly(mask, fromPT, color=1)
+    #mask_from = mask
+    mask_from = mask.astype(bool)
+    
+    x_map_ref = np.tile(np.arange(720), [720, 1])
+    y_map_ref = np.tile(np.arange(720), [720, 1]).T
+    x_map_from = np.tile(np.arange(720), [720, 1])
+    y_map_from = np.tile(np.arange(720), [720, 1]).T
+    
+    count = x_map_from[mask_ref].shape[0]
+    srcPts = np.zeros([count, 2])
+    srcPts[:, 0] = x_map_ref[mask_ref][:]
+    srcPts[:, 1] = y_map_ref[mask_ref][:]
+    #print srcPts
+    
+    #srcPts = np.array([[240, 210], [120, 105]])
+    solver = MLSQ.MovingLSQ_2D(refPT, fromPT)
+    dstPts = solver.Run2(srcPts, alpha = 1)
+
+    #exit()
+    #print (dstPts - srcPts)
+    #print dstPts[-1, :]
+    #exit()
+    #dstPts = np.load('dstPts.npy')
+    mask[:, :] = 0
+    x_mask = mask.copy().astype(np.float)
+    y_mask = mask.copy().astype(np.float)
+    x_mask[mask_ref] = dstPts[:, 0]
+    y_mask[mask_ref] = dstPts[:, 1]
+    #print x_mask
+    img = cv2.remap(origin, x_mask.astype(np.float32), y_mask.astype(np.float32), cv2.INTER_CUBIC)
+    #x_mask[]
+    #dstPts = solver.Run(srcPts)
+    #np.save('dstPts.npy', dstPts)
+    #exit()
     for pt in refPT:
         cv2.circle(ref, (pt[0], pt[1]), 3, (0, 255, 0))
 
-    for pt in pts:
-        cv2.circle(ref, (pt[0], pt[1]), 3, (0, 0, 255))
 
-    for pt in fromPT:
-        cv2.circle(origin, (pt[0], pt[1]), 3, (0, 255, 0))
+    for pt in range(count):
+        cv2.circle(origin, (int(dstPts[pt, 0]), int(dstPts[pt, 1])), 2, (0, 255, 0))
     
     #test = cv2.warpPerspective(origin, H, (720, 720), flags=(cv2.INTER_CUBIC + cv2.WARP_INVERSE_MAP))
 
     cv2.namedWindow('GG')
-    cv2.imshow('GG', ref)
+    cv2.imshow('GG', img)
     cv2.waitKey(0)
 
